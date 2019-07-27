b458213306c710ebae2d603f6d18fc86ce6a2317
CUber Dude <jain.gaurav6@tcs.com>
2019-07-27 17:44:58 +0530

    basic chat app

---
+++ main.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, redirect, session, request
+from flask_socketio import SocketIO,emit, join_room, leave_room
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'a02f6b6f061fb082059c'
+
+socketio = SocketIO(app)
+
+@socketio.on('joined', namespace='/chat')
+def joined(message):
+    room = "public room"
+    join_room(room)
+    emit('status', {'msg': "someone" + ' has entered the room.'}, room=room)
+
+
+@socketio.on('text', namespace='/chat')
+def text(message):
+    room = "public room"
+    emit('message', {'msg': session.get('name') + ':' + message['msg']}, room=room)
+
+
+@socketio.on('left', namespace='/chat')
+def left(message):
+    room = "public room"
+    leave_room(room)
+    emit('status', {'msg': "someone" + ' has left the room.'}, room=room)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def indexPost():
+    session['name'] = request.form['name']
+    return redirect("/chat")
+
+@app.route('/chat', methods=['GET'])
+def chat():
+    name = session.get('name')
+    room = "public room"
+    return render_template('chat.html', name=name, room=room)
+
+if __name__ == '__main__':
+    socketio.run(app)
