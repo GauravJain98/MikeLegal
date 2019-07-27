b5e954becc1b1d83a83b606855a5dcb303eb488e
CUber Dude <jain.gaurav6@tcs.com>
2019-07-28 01:53:13 +0530

    nltk.txt

--- convert.py
+++ convert.py
@@ -1,8 +1,6 @@
 import nltk
 
 def htmlTokenize(message):
-    nltk.download('punkt')
-    nltk.download('averaged_perceptron_tagger')
     text = nltk.pos_tag(nltk.word_tokenize(message))
     return str(text)
 
